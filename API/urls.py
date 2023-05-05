f5878574a520eece310a05e0094de1b699e86859
Ander <Anderson_ortiz05@hotmail.com>
2023-05-04 22:44:25 -0400

    cambio rapido

--- API/urls.py
+++ API/urls.py
@@ -5,5 +5,5 @@ from .responses.RegisterResponse import RegisterFull
 urlpatterns = [
     path('PatientBasic/<str:cedulaP>',patientBasic),#http://127.0.0.1:8000/API/PatientBasic/cedula
     path('loginBasic/<str:usern>/<str:passw>',LoginBasic),#http://127.0.0.1:8000/API/loginBasic/username/pass
-    path('RegisterClient/<int:idC>', RegisterFull),
+    path('RegisterClient/<int:idC>', RegisterFull),#http://127.0.0.1:8000/API/RegisterClient/ID
 ]
